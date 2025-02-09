8042147d5c4477f0ecccf749fb985860c81e0cd5
lmt610 <147426242+lmt610@users.noreply.github.com>
2025-02-09 06:20:21 +0000

    Update admin.py
    Can view/change points now. Login details in comment.

--- project/users/admin.py
+++ project/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile
+# username: admin, password: password
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'other_field', 'points')
+    search_fields = ('user__username', 'other_field')
+    list_filter = ('user__is_active', 'user__is_staff')
 
-# Register your models here.
+admin.site.register(Profile, ProfileAdmin)
