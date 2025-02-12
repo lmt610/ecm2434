23750aea8b5990a46a5c9cadf02a396f2169da3b
Aria 4G <111776997+Aura4G@users.noreply.github.com>
2025-02-12 17:36:51 +0000

    Update models.py
    created location and race models

--- project/race/models.py
+++ project/race/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
 
-# Create your models here.
+
+class Race(models.Model):
+    title = models.CharField(max_length=255)
+    start = models.ForeignKey('Location', related_name='start_races', on_delete=models.CASCADE)
+    end = models.ForeignKey('Location', related_name='end_races', on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    is_complete = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+    
+    def get_duration(self):
+        return self.end_time - self.start_time
+
+    def get_duration_in_minutes(self):
+        duration = self.get_duration()
+        return duration.total_seconds() / 60
+    
+    @property
+    def race_date(self):
+        return self.start_time.date()  # This extracts the date part from the start_time
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
