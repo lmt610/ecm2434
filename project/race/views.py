05767b37d7c530a172da40d4db2f76110c58aca9
Alex Norris <alexwnorris@gmail.com>
2025-02-13 15:25:07 +0000

    adjusted race app view to pass in race DB to page

--- project/race/views.py
+++ project/race/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
-
-def race_view(request):
-    return render(request,"race/race.html")
+from django.shortcuts import render, get_object_or_404
+from .models import Race
+def race_view(request,race_id):
+    race = get_object_or_404(Race, id=race_id)
+    return render(request,"race/race.html", {"race": race})
