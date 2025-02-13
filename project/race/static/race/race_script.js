379679b2d96f50f40b56ee1e429065b3c2e5f13f
Aura4G <111776997+Aura4G@users.noreply.github.com>
2025-02-13 12:48:27 +0000

    added proximity condition with haversine to views and js file

--- project/race/static/race/race_script.js
+++ project/race/static/race/race_script.js
@@ -18,3 +18,36 @@ function resetRace() {
 function initMap() {
     console.log("Map Initialized"); // Replace with Google Maps or Leaflet.js
 }
+
+
+//tracking user location
+const x = document.getElementById("locationDisplay");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+}
+
+function showError(error) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        x.innerHTML = "User denied the request for Geolocation."
+        break;
+      case error.POSITION_UNAVAILABLE:
+        x.innerHTML = "Location information is unavailable."
+        break;
+      case error.TIMEOUT:
+        x.innerHTML = "The request to get user location timed out."
+        break;
+      case error.UNKNOWN_ERROR:
+        x.innerHTML = "An unknown error occurred."
+        break;
+    }
+} 
--- project/race/views.py
+++ project/race/views.py
@@ -1,4 +1,36 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from math import radians, sin, cos, sqrt, atan2
 
 def race_view(request):
     return render(request,"race/race.html")
+
+#haversine formula to calculate distance
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371 #radius of earth in kilometers
+    dlat = radians(lat2 - lat1)
+    dlon = radians(lon2 - lon1)
+    a = sin(dlat / 2) ** 2 + cos(radians(lat1)) * cos(radians(lat2)) * sin(dlon/2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1-a))
+    return R * c
+
+@csrf_exempt #change later to properly account for Cross Site Request Forgery
+def calculate_distance(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        user_lat, user_lon = float(data["latitude"]), float(data["longitude"])
+
+        place = Location.objects.first()
+        place_lat, place_lon = place.latitud, place.longitude
+
+        distance = haversine(user_lat, user_lon, place_lat, place_lon)
+
+        threshold = 5
+        if distance <= threshold:
+            return JsonResponse({"status": "within range", "distance": round(distance, 2)})
+        else:
+            return JsonResponse({"status": "within range", "distance": round(distance, 2)})
+        
+    return JsonResponse({"error": "Invalid request"}, status = 400)
