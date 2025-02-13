0887900470fe1c37c57a94896d297bbb9a947ba4
Alex Norris <alexwnorris@gmail.com>
2025-02-13 15:25:29 +0000

    adjusted race urls file to include id of race in url

--- project/race/urls.py
+++ project/race/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.race_view, name='race'),
+    path('<int:race_id>/', views.race_view, name='race_detail'),
 ]
