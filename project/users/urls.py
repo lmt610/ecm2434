964c57534cd097e8ce783bcde9f7d2b1fc6d4dff
Dan <danjloveless@gmail.com>
2025-02-14 16:14:16 +0000

    Settings page front end complete. Need to add back end functionality to save settings to database. gitignore added to ignore venv and pycache files.

--- project/users/urls.py
+++ project/users/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
 from . import views
-from .views import home, register, sign_in, welcome, increment_points
+from .views import home, register, sign_in, welcome, increment_points, settings_view, delete_account, change_password, toggle_setting
 urlpatterns = [   
     path('', home, name='home'),
-    path('login/', views.sign_in, name='login'),
+    path('login/', sign_in, name='login'),
     path('register/', register, name='register'),
     path('welcome/', welcome, name='welcome'),
     path('tasks/', include('tasks.urls')),
     path('increment-points/', increment_points, name='increment_points'),
+    path('settings/', settings_view, name='settings'),
+    path('change-password/', change_password, name='change_password'),
+    path('delete-account/', delete_account, name='delete_account'),
+    path('toggle-setting/', toggle_setting, name='toggle_setting')
     ]
