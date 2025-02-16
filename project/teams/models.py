bb0e9ad21de63a4ff212493be5a89de0304a0d5a
lmt610 <147426242+lmt610@users.noreply.github.com>
2025-02-16 13:45:08 +0000

    teams logic
    still working on templates, will upload asap

---
+++ project/teams/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.conf import settings
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    admin = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='admin_of', on_delete=models.CASCADE)
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='teams', blank=True)
+    points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class TeamJoinRequest(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=(
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+    ), default='pending')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f'Join request for {self.user.username} to {self.team.name}'
