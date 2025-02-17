d21423ceba3cf77975972ee0ff9a98bd5dff9ead
Keates <chrisextra185@gmail.com>
2025-02-17 18:54:31 +0000

    add more tests to login page

--- project/users/tests/test_views.py
+++ project/users/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.urls import reverse
+from ..forms import LoginForm
 
 class LoginViewTest(TestCase):
     __USER_NAME = "testUser"
@@ -10,6 +11,12 @@ class LoginViewTest(TestCase):
         self.client = Client()
         self.user = User.objects.create_user(username=self.__USER_NAME, password=self.__USER_PASSWORD)
 
+    def test_get_page(self):
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context['form'], LoginForm)
+        self.assertFalse(response.context['form'].is_bound) 
+
     def test_valid_sign_in(self):
         response = self.client.post("/login/", {"username": self.__USER_NAME, "password":self.__USER_PASSWORD})
         self.assertEqual(response.status_code, 302)
@@ -18,5 +25,23 @@ class LoginViewTest(TestCase):
 
     def test_wrong_username(self):
         response = self.client.post("/login/", {"username": "wrongUsername", "password":self.__USER_PASSWORD})
+        self.assert_login_failed(response)        
+
+    def test_wrong_password(self):
+        response = self.client.post("/login/", {"username": self.__USER_NAME, "password":"wrongPassword"})
+        self.assert_login_failed(response)
+
+    def test_wrong_form_format(self):
+        wrong_forms = [
+            {},
+            {"otherAttrib":12},
+            {"usr":self.__USER_NAME, "password": self.__USER_PASSWORD},
+            {"username":["harry", 12], "password": {"random":["james", "simon"]}}
+        ] 
+        for form in wrong_forms:
+            response = self.client.post("/login/", form)
+            self.assert_login_failed(response) 
+
+    def assert_login_failed(self, response):
         self.assertEqual(response.status_code, 200)
         self.assertFalse(response.wsgi_request.user.is_authenticated)
