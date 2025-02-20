ca1b0c5ead67b533c7468c1e9e453ab596fea6c1
lmt610 <147426242+lmt610@users.noreply.github.com>
2025-02-20 19:36:19 +0000

    Update urls.py

--- project/race/urls.py
+++ project/race/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.race_view, name='race'),
-    path('leaderboard/', views.leaderboard, name='leaderboard'),
+    path('leaderboard/', views.leaderboard, name='leaderboard'),    
+    path('leaderboard/', views.leaderboard_view, name='leaderboard_view'), 
+
 ]
