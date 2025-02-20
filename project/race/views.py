d71a7872f0471c8d0d564d7d4965cd1ab5922d30
lmt610 <147426242+lmt610@users.noreply.github.com>
2025-02-20 19:34:57 +0000

    Update views.py

--- project/race/views.py
+++ project/race/views.py
@@ -63,3 +63,21 @@ def leaderboard(request):
         for entry in leaderboard_entries
     ]
     return JsonResponse({"leaderboard": data})
+
+
+def leaderboard_view(request):
+    race_title = request.GET.get("race_title")
+    if race_title:
+                # race title search is case insensitive
+        leaderboard_entries = LeaderboardEntry.objects.filter(race__title__icontains=race_title).order_by('completion_time').select_related('user', 'race')
+    else:
+        leaderboard_entries = LeaderboardEntry.objects.order_by('completion_time').select_related('user', 'race')
+        
+    top_entries = leaderboard_entries[:10]
+    entries_count = leaderboard_entries.count()
+
+    context = {
+        'top_entries': top_entries,
+    }
+    return render(request, 'race/leaderboard.html', context)
+
