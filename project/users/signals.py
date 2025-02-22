67df92e2f84f075dcd07b8e72a0328c98c4a210d
Alex Norris <alexwnorris@gmail.com>
2025-02-22 16:17:27 +0000

    Test github actions

--- project/users/signals.py
+++ project/users/signals.py
@@ -13,11 +13,3 @@ def populate_database(sender, **kwargs):
     if sender.name != "users":
         return
     
-    # Create a default user if it doesn't exist
-    if not User.objects.filter(username="UserA").exists():
-        user = User.objects.create_user(username="UserA", password="Password")
-        profile, created = Profile.objects.get_or_create(user=user, other_field="12345678", points=0)
-
-    if not User.objects.filter(username="UserB").exists():
-        user = User.objects.create_user(username="UserB", password="Password")
-        profile, created = Profile.objects.get_or_create(user=user, other_field="12345678", points=0)
