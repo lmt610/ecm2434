35414b058b71328d5736325f3d28dbe608e11fda
Dan <danjloveless@gmail.com>
2025-02-23 15:04:59 +0000

    Universal header created for all pages Header applied to some pages
    Login and register pages error styles resolved

--- project/ecm2434/urls.py
+++ project/ecm2434/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('race/', include('race.urls')),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     ]
