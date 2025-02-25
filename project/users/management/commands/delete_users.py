a534cd1840da664270a34b5c539a228efeb5d02c
lmt610 <147426242+lmt610@users.noreply.github.com>
2025-02-25 06:04:38 +0000

    Update delete_users.py

--- project/users/management/commands/delete_users.py
+++ project/users/management/commands/delete_users.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from race.models import Race, Location, LeaderboardEntry
-from django.utils import timezone  # Import timezone
+from django.utils import timezone  
 
 class Command(BaseCommand):
-    help = "Deletes all races, users, locations, and leaderboard entries."
+    help = "Deletes users."
 
     def handle(self, *args, **kwargs):
         superuser_exists = User.objects.filter(is_superuser=True).exists()
@@ -16,5 +16,5 @@ class Command(BaseCommand):
             num_deleted_users, _ = User.objects.all().delete()
             self.stdout.write(self.style.SUCCESS(f"Deleted {num_deleted_users} users."))
 
-        self.stdout.write(self.style.SUCCESS("All races, users, locations, and leaderboard entries have been deleted"))
+        self.stdout.write(self.style.SUCCESS("All users have been deleted"))
 
