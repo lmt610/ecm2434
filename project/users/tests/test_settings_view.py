2a552c1ddf601bfb7ed05cf66af42f02f7313010
Keates <chrisextra185@gmail.com>
2025-03-05 22:02:55 +0000

    add more settings tests

--- project/users/tests/test_settings_view.py
+++ project/users/tests/test_settings_view.py
@@ -2,21 +2,38 @@ from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from bs4 import BeautifulSoup
 from ..models import UserSettings
+from django.urls import reverse
+from django.contrib.messages import get_messages
 
 class SettingsViewTest(TestCase):
+    __USER_PASSWORD = "testpassword"
+
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(username="testuser12", password="testpassword")
+        self.user = User.objects.create_user(username="testuser12", password=self.__USER_PASSWORD)
         self.client.login(username="testuser12", password="testpassword")
 
     def test_valid_change_password(self):
-        pass
-
-    def test_invalid_change_password(self):
-        pass
+        form = {
+            "old_password": self.__USER_PASSWORD,
+            "new_password1": "newvalidpassword",
+            "new_password2": "newvalidpassword"
+        }
+        response = self.client.post("/change-password/", form)
+       
+        self.assertEqual(response.json()["message"], "Your password was successfully updated!")
+        self.assertFalse(User.objects.filter(password=self.user.password).exists())
+        self.assertTrue(User.objects.filter(username=self.user.username).exists())
 
     def test_delete_account(self):
-        pass
+        response = self.client.post("/delete-account/")
+        messages = list(get_messages(response.wsgi_request))
+        
+        self.assertEqual(len(messages),1)
+        self.assertEqual(str(messages[0]), "Your account has been successfully deleted.")
+        self.assertFalse(User.objects.filter(username=self.user.username).exists())
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse("home"))
 
     def test_toggle_setting_valid(self):
         attributes = [field.name for field in UserSettings._meta.get_fields() if field.name not in ["id","user"]]
