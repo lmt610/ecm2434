30b82d0856c3623161be255791c628ac53120e06
Keates <chrisextra185@gmail.com>
2025-03-06 12:32:15 +0000

    added user settings to admin page

--- project/users/admin.py
+++ project/users/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, UserSettings
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'other_field', 'points')
     search_fields = ('user__username', 'other_field')
     list_filter = ('user__is_active', 'user__is_staff')
 
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(UserSettings)
