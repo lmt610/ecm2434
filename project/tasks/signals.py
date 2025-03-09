282e81cc8d6f22549880662f077562c343db51cf
lmt610 <147426242+lmt610@users.noreply.github.com>
2025-03-09 21:26:07 +0000

    Update signals.py
    removed debugging print statements

--- project/tasks/signals.py
+++ project/tasks/signals.py
@@ -6,15 +6,12 @@ from users.models import Profile
 
 @receiver(post_save, sender=RaceEntry)
 def check_task_completion(sender, instance, created, **kwargs):
-    print("running")
     if created:
-        print("created")
         tasks = Task.objects.all()
         for task in tasks:
             if not task.is_active():
                 continue
             if task.task_type == 'multi':
-                print("multi")
                 if (int(task.completed_races_count(instance.user)) >= int(task.required_races)):
                     if not UserTaskCompletion.objects.filter(user=instance.user, task=task).exists():
                         profile = Profile.objects.get(user=instance.user)
@@ -22,13 +19,11 @@ def check_task_completion(sender, instance, created, **kwargs):
                         profile.save()
                         # prevents the user from regaining points for completing the same task
                         UserTaskCompletion.objects.create(user=instance.user, task=task)
-                        print("created for multi")
             elif task.task_type == 'single':
-                print("single")
                 if task.is_completed_by_user(instance.user):
                     if not UserTaskCompletion.objects.filter(user=instance.user, task=task).exists():
                         profile = Profile.objects.get(user=instance.user)
                         profile.points += task.points_awarded
                         profile.save()
                         UserTaskCompletion.objects.create(user=instance.user, task=task)
-                        print("created for single")
+                    
