0097f12756dbbd7538c1f0c40bb12aefcb6fe41b
Alex Norris <alexwnorris@gmail.com>
2025-03-24 12:04:58 +0000

    stopped duplicate achievements,

--- project/achievements/signals.py
+++ project/achievements/signals.py
@@ -19,7 +19,7 @@ def populate_database_with_achievements(sender, **kwargs):
     ############
     ## RACE
     ############
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
             title="A Whole New World",
             description="Run a race",
             main_condition_model="COUNT_RACES",
@@ -27,7 +27,7 @@ def populate_database_with_achievements(sender, **kwargs):
             main_condition_value="0",
             subconditions=[]
         )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
             title="Rising Star",
             description="Run 10 races",
             main_condition_model="COUNT_RACES",
@@ -35,7 +35,7 @@ def populate_database_with_achievements(sender, **kwargs):
             main_condition_value="9",
             subconditions=[]
         )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
             title="Seasoned Veteran",
             description="Run 100 races",
             main_condition_model="COUNT_RACES",
@@ -43,7 +43,7 @@ def populate_database_with_achievements(sender, **kwargs):
             main_condition_value="99",
             subconditions=[]
         )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
             title="Bronze Beginnings",
             description="Get a bronze medal",
             main_condition_model="COUNT_RACES",
@@ -53,7 +53,7 @@ def populate_database_with_achievements(sender, **kwargs):
                 ["medal", "=", "bronze"]
             ]
     )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
             title="Silvery Success",
             description="Get a silver medal",
             main_condition_model="COUNT_RACES",
@@ -63,7 +63,7 @@ def populate_database_with_achievements(sender, **kwargs):
                 ["medal", "=", "silver"]
             ]
     )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
             title="Golden Glory",
             description="Get a gold medal",
             main_condition_model="COUNT_RACES",
@@ -73,7 +73,7 @@ def populate_database_with_achievements(sender, **kwargs):
                 ["medal", "=", "gold"]
             ]
         )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
             title="Sprinter",
             description="Complete 2 races in under 1 minute",
             main_condition_model="COUNT_RACES",
@@ -83,7 +83,7 @@ def populate_database_with_achievements(sender, **kwargs):
                 ["duration", "<", "60"]
             ]
         )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
             title="The Long Game",
             description="Complete 10 races with a distance over 0.5km",
             main_condition_model="COUNT_RACES",
@@ -93,7 +93,7 @@ def populate_database_with_achievements(sender, **kwargs):
                 ["distance", ">", "0.5"]
             ]
         )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
             title="Are We There Yet?",
             description="Complete a race longer than 1km",
             main_condition_model="COUNT_RACES",
@@ -103,7 +103,7 @@ def populate_database_with_achievements(sender, **kwargs):
                 ["distance", ">", "1.0"]
             ]
         )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
             title="Marathon Master",
             description="Complete 10 races longer than 1km",
             main_condition_model="COUNT_RACES",
@@ -113,7 +113,7 @@ def populate_database_with_achievements(sender, **kwargs):
                 ["distance", ">", "1.0"]
             ]
         )
-    Achievement.objects.create(
+    Achievement.objects.creget_or_createate(
             title="Double Crown",
             description="Hold the top leaderboard position on two races",
             main_condition_model="COUNT_RACES",
@@ -123,7 +123,7 @@ def populate_database_with_achievements(sender, **kwargs):
                 ["position", "=", "1"]
             ]
         )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
             title="Deja Vu",
             description="Complete the same route 5 times",
             main_condition_model="COUNT_RACES",
@@ -133,7 +133,7 @@ def populate_database_with_achievements(sender, **kwargs):
                 ["number_of_completions", ">", "4"]
             ]
         )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
             title="Can't Stop, Won't Stop",
             description="Complete the same route 10 times",
             main_condition_model="COUNT_RACES",
@@ -143,7 +143,7 @@ def populate_database_with_achievements(sender, **kwargs):
                 ["number_of_completions", ">", "19"]
             ]
         )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
             title="Tour Guide",
             description="Complete the same route 50 times",
             main_condition_model="COUNT_RACES",
@@ -153,7 +153,7 @@ def populate_database_with_achievements(sender, **kwargs):
                 ["number_of_completions", ">", "49"]
             ]
         )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
         title="Peak Performance",
         description="Get a gold medal on an uphill race",
         main_condition_model="COUNT_RACES",
@@ -164,7 +164,7 @@ def populate_database_with_achievements(sender, **kwargs):
             ["medal", "=", "gold"]
         ]
     )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
         title="Summit to be Proud Of",
         description="Complete 5 uphill races",
         main_condition_model="COUNT_RACES",
@@ -174,7 +174,7 @@ def populate_database_with_achievements(sender, **kwargs):
             ["tags", "contains", "uphill"],
         ]
     )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
         title="Summit Special",
         description="Complete 10 uphill races",
         main_condition_model="COUNT_RACES",
@@ -184,7 +184,7 @@ def populate_database_with_achievements(sender, **kwargs):
             ["tags", "contains", "uphill"],
         ]
     )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
         title="Break a Leg",
         description="Complete 100 uphill races",
         main_condition_model="COUNT_RACES",
@@ -197,7 +197,7 @@ def populate_database_with_achievements(sender, **kwargs):
     ############
     ## TEAM
     ############
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
         title="Branching Out",
         description="Join a team",
         main_condition_model="COUNT_TEAMS",
@@ -205,7 +205,7 @@ def populate_database_with_achievements(sender, **kwargs):
         main_condition_value="0",
         subconditions=[]
     )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
         title="Leafy Legion",
         description="Be part of a team with 20 members",
         main_condition_model="COUNT_TEAMS",
@@ -215,7 +215,7 @@ def populate_database_with_achievements(sender, **kwargs):
             ["number_of_members", ">", "19"]
         ]
     )
-    Achievement.objects.create(
+    Achievement.objects.get_or_create(
         title="Playing the Field",
         description="Be a member of 5 teams",
         main_condition_model="COUNT_TEAMS",
