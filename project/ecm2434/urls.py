fb91cf7a5456bfbd0de7a1dace31dcd29e94a190
Keates <chrisextra185@gmail.com>
2025-03-25 21:04:19 +0000

    remove cloudinary media url from urls

--- project/ecm2434/urls.py
+++ project/ecm2434/urls.py
@@ -17,8 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from django.conf import settings
-from django.conf.url.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +26,3 @@ urlpatterns = [
     path('leaderboard/', include('leaderboard.urls')),
     path('achievements/', include('achievements.urls'))
     ]
-
-if not settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
